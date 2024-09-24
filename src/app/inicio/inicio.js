4cce7c1fd5895c52a313406f8db49d904d55f9d0
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2024-09-23 23:28:10 -0300

    chore: Modificação na responividade smartpfhone

--- src/app/inicio/inicio.js
+++ src/app/inicio/inicio.js
@@ -1,2 +1,16 @@
 
 
+// FUNÇÃO PARA BAIXAR O CV
+function baixarCV(){
+    const alerta = confirm('Deseja baixar o Curriculo do desenvolvedor?');
+    if(alerta == true){
+        const link = document.createElement('a');
+        link.href = 'https://drive.google.com/uc?export=download&id=1sEQiuiZWSWkV2MxaHqLsGsdw6obs8Xos';
+        link.download = 'Curriculo.pdf'; // Nome sugerido para o arquivo
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link); // Remove o link após o clique
+    } else {
+        return;
+    }
+}
